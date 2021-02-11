9ea1418eea0f6076e46622e103f8b28ddceb3b3e
Matt Malone <mattagape@me.com>
2021-02-11 14:48:10 +0700

    Set up some Types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,11 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :links, [Types::LinkType], null: false, description: 'A list of links'
+
+    def links
+      Link.order(created_at: :desc)
     end
+    
   end
 end
