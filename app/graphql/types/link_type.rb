44e24cac3f81d374203315393ef21aee4b3db7f7
Matt Malone <mattagape@me.com>
2021-02-17 13:59:13 +0700

    Added comment

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -1,3 +1,5 @@
+# See this about optimizing it for production: 
+# https://evilmartians.com/chronicles/graphql-on-rails-1-from-zero-to-the-first-query
 module Types
   class LinkType < Types::BaseObject
     field :id, ID, null: false
